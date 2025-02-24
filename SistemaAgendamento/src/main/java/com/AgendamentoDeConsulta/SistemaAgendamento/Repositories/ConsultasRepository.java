eba0b57cac4acc95a3c973996f2dfb426243fbce
RocketCompanny <financeiro@rocketcompanny.com.br>
2025-02-23 22:05:27 -0300

    feat: Adicionando Decorador @Repository.

--- SistemaAgendamento/src/main/java/com/AgendamentoDeConsulta/SistemaAgendamento/Repositories/ConsultasRepository.java
+++ SistemaAgendamento/src/main/java/com/AgendamentoDeConsulta/SistemaAgendamento/Repositories/ConsultasRepository.java
@@ -2,6 +2,9 @@ package com.AgendamentoDeConsulta.SistemaAgendamento.Repositories;
 
 import com.AgendamentoDeConsulta.SistemaAgendamento.Models.Consultas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConsultasRepository extends JpaRepository<Consultas, Long> {
+
 }
